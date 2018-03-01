40987fbf26449e9312d399c20a39d925c6a2f3fb
Matt Baker <matthew.reiss.baker@gmail.com>
2018-03-01 05:38:39 -0800

    Run ssh commands in same path

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #[macro_use]
 extern crate structopt;
 extern crate xdg;
+extern crate pathdiff;
+extern crate shell_escape;
+extern crate isatty;
 
 mod cli;
 mod conn;
@@ -8,7 +11,9 @@ mod errors;
 mod process;
 
 use structopt::StructOpt;
+use std::env;
 use std::path::PathBuf;
+use std::borrow::Cow;
 use cli::{SlinkCommand, RsyncDirection};
 use errors::SlinkResult;
 
@@ -39,12 +44,54 @@ fn use_host(host: String) -> SlinkResult<()> {
 }
 
 fn go() -> SlinkResult<()> {
-    conn::ssh_command(|_| {})
+    conn::ssh_command(|ssh| {
+        ssh.arg(exec_shell_in_same_path());
+    })
+}
+
+fn exec_command_in_same_path(command: &str) -> String {
+    let home_dir = env::home_dir().unwrap();
+    let pwd = env::current_dir().unwrap();
+    match pathdiff::diff_paths(&pwd, &home_dir) {
+        Some(relative_path) => {
+            let rel_str = relative_path.to_str().unwrap();
+            exec_command_in(rel_str, command)
+        },
+        None => {
+            let pwd_str = pwd.to_str().unwrap();
+            exec_command_in(pwd_str, command)
+        }
+    }
+}
+
+fn exec_shell_in_same_path() -> String {
+    exec_command_in_same_path("$SHELL --login")
+}
+
+fn exec_command_in(path: &str, command: &str) -> String {
+    let escaped = shell_escape::escape(Cow::Borrowed(path));
+
+    // Escape the echo message separately, since otherwise you'd need to encase
+    // in quotes (which would break shell escaping)
+    let echo_string = format!("Running in directory: {}", path);
+    let escaped_echo = shell_escape::escape(Cow::Borrowed(echo_string.as_str()));
+
+    format!(
+        "test -d {} {} && cd {} ; exec {}",
+        escaped,
+        if isatty::stdout_isatty() {
+            format!("&& echo {}", escaped_echo)
+        } else {
+            String::new()
+        },
+        escaped,
+        command
+    )
 }
 
 fn run(command: String) -> SlinkResult<()> {
     conn::ssh_command(|ssh| {
-        ssh.arg(command);
+        ssh.arg(exec_command_in_same_path(command.as_str()));
     })
 }
 
