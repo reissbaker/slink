167c5a6733d89a00fec1824cb3c6d257682a722e
Matt Baker <matthew.reiss.baker@gmail.com>
2018-03-09 02:43:21 -0800

    Clean up code with and_then

--- src/paths.rs
+++ src/paths.rs
@@ -10,13 +10,10 @@ pub fn same_path() -> PathBuf {
 }
 
 pub fn relative_pwd() -> Option<PathBuf> {
-    match env::home_dir() {
-        Some(home_dir) => {
-            let pwd = pwd_or_panic();
-            pathdiff::diff_paths(&pwd, &home_dir)
-        },
-        None => None,
-    }
+    env::home_dir().and_then(|home_dir| {
+        let pwd = pwd_or_panic();
+        pathdiff::diff_paths(&pwd, &home_dir)
+    })
 }
 
 pub fn pwd_or_panic() -> PathBuf {
